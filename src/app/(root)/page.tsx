b8a3afe397e677f8660ab98aa17241b2d20fa316
Manuel Tati <manuel.tati@minfin.gov.ao>
2023-11-20 22:12:05 +0000

    feat:text changed

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-[url('/assets/63148.jpg')] bg-cover">
       <div className="w-fit flex flex-col justify-center items-center space-y-1">
-        <h1 className="text-7xl uppercase font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-900 to-white">
-          manuel tati
+        <h1 className="text-7xl uppercase font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-800 to-white">
+          EM CONSTRUÇÃO
         </h1>
         <span className="self-end text-sm text-white font-semibold">
           Apenas se inspirando
