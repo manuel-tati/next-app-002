c0cc370bbccd97b72ac12be5f1e7506bd07a7ae0
Manuel Tati <manuel.tati@minfin.gov.ao>
2023-11-20 22:00:54 +0000

    feat:changed project

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-[url('/assets/63148.jpg')] bg-cover">
+      <div className="w-fit flex flex-col justify-center items-center space-y-1">
+        <h1 className="text-7xl uppercase font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-900 to-white">
+          manuel tati
+        </h1>
+        <span className="self-end text-sm text-white font-semibold">
+          Apenas seja criativo nas suas imaginações
+        </span>
+      </div>
+    </div>
+  );
+}
